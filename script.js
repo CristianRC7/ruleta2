da455b7d02d503e2eb5c2d45c0a029b57d1450e4
Cristian Ramirez <cristian25ramirezrc@gmail.com>
2024-04-03 23:28:54 -0400

    cambios de historial

--- script.js
+++ script.js
@@ -1,197 +1,182 @@
 var numQuestions = 10;
-    var data = [
-      {
-        "label": "Pregunta 1",
-        "value": 1,
-        "question": "¿Qué predice la teoría de la relatividad general sobre el comienzo del universo, según las soluciones de Friedmann?"
-      },
-      {
-        "label": "Pregunta 2",
-        "value": 2,
-        "question": "¿Qué demostraron Penrose y Hawking sobre las singularidades y el inicio del universo en su trabajo conjunto?"
-      },
-      {
-        "label": "Pregunta 3",
-        "value": 3,
-        "question": "¿Qué es el principio de exclusión de Pauli?"
-      },
-      {
-        "label": "Pregunta 4",
-        "value": 4,
-        "question": "¿Qué calculó Chandrasekhar durante su viaje a Inglaterra en 1928?"
-      },
-      {
-        "label": "Pregunta 5",
-        "value": 5,
-        "question": "¿Qué inspiró a Hawking a pensar en los agujeros negros una noche de noviembre de 1970?"
-      },
-      {
-        "label": "Pregunta 6",
-        "value": 6,
-        "question": "¿Cómo comparó Hawking el horizonte de sucesos de un agujero negro con el borde de una sombra?"
-      },
-      {
-        "label": "Pregunta 7",
-        "value": 7,
-        "question": "¿Qué fenómeno físico se relaciona con el comportamiento no decreciente del área de un agujero negro?"
-      },
-      {
-        "label": "Pregunta 8",
-        "value": 8,
-        "question": "¿Qué sugirió Jacob Bekenstein sobre el área del horizonte de sucesos de un agujero negro?"
-      },
-      {
-        "label": "Pregunta 9",
-        "value": 9,
-        "question": "¿Quiénes fueron los dos expertos soviéticos con los que Stephen Hawking discutió sobre agujeros negros en septiembre de 1973?"
-      },
-      {
-        "label": "Pregunta 10",
-        "value": 10,
-        "question": "¿Qué efecto tienen las fluctuaciones cuánticas en el espacio vacío según el principio de incertidumbre?"
-      }
-    ];
+var data = [
+  {
+    "label": "Pregunta 1",
+    "value": 1,
+    "question": "¿Qué predice la teoría de la relatividad general sobre el comienzo del universo, según las soluciones de Friedmann?"
+  },
+  {
+    "label": "Pregunta 2",
+    "value": 2,
+    "question": "¿Qué demostraron Penrose y Hawking sobre las singularidades y el inicio del universo en su trabajo conjunto?"
+  },
+  {
+    "label": "Pregunta 3",
+    "value": 3,
+    "question": "¿Qué es el principio de exclusión de Pauli?"
+  },
+  {
+    "label": "Pregunta 4",
+    "value": 4,
+    "question": "¿Qué calculó Chandrasekhar durante su viaje a Inglaterra en 1928?"
+  },
+  {
+    "label": "Pregunta 5",
+    "value": 5,
+    "question": "¿Qué inspiró a Hawking a pensar en los agujeros negros una noche de noviembre de 1970?"
+  },
+  {
+    "label": "Pregunta 6",
+    "value": 6,
+    "question": "¿Cómo comparó Hawking el horizonte de sucesos de un agujero negro con el borde de una sombra?"
+  },
+  {
+    "label": "Pregunta 7",
+    "value": 7,
+    "question": "¿Qué fenómeno físico se relaciona con el comportamiento no decreciente del área de un agujero negro?"
+  },
+  {
+    "label": "Pregunta 8",
+    "value": 8,
+    "question": "¿Qué sugirió Jacob Bekenstein sobre el área del horizonte de sucesos de un agujero negro?"
+  },
+  {
+    "label": "Pregunta 9",
+    "value": 9,
+    "question": "¿Quiénes fueron los dos expertos soviéticos con los que Stephen Hawking discutió sobre agujeros negros en septiembre de 1973?"
+  },
+  {
+    "label": "Pregunta 10",
+    "value": 10,
+    "question": "¿Qué efecto tienen las fluctuaciones cuánticas en el espacio vacío según el principio de incertidumbre?"
+  }
+];
 
-    var padding = {top:20, right:40, bottom:0, left:0},
-        w = 500 - padding.left - padding.right,
-        h = 500 - padding.top  - padding.bottom,
-        r = Math.min(w, h)/2,
-        rotation = 0,
-        oldrotation = 0,
-        picked = 100000,
-        oldpick = [],
-        color = d3.scale.category20();
+var padding = {top:20, right:40, bottom:0, left:0},
+    w = 500 - padding.left - padding.right,
+    h = 500 - padding.top  - padding.bottom,
+    r = Math.min(w, h)/2,
+    rotation = 0,
+    oldrotation = 0,
+    picked = 100000,
+    oldpick = [],
+    color = d3.scale.category20();
 
-    var svg = d3.select('#chart')
-        .append("svg")
-        .data([data])
-        .attr("width",  w + padding.left + padding.right)
-        .attr("height", h + padding.top + padding.bottom);
+var svg = d3.select('#chart')
+    .append("svg")
+    .data([data])
+    .attr("width",  w + padding.left + padding.right)
+    .attr("height", h + padding.top + padding.bottom);
 
-    var container = svg.append("g")
-        .attr("class", "chartholder")
-        .attr("transform", "translate(" + (w/2 + padding.left) + "," + (h/2 + padding.top) + ")");
-    var vis = container
-        .append("g");
+var container = svg.append("g")
+    .attr("class", "chartholder")
+    .attr("transform", "translate(" + (w/2 + padding.left) + "," + (h/2 + padding.top) + ")");
+var vis = container
+    .append("g");
 
-    var pie = d3.layout.pie().sort(null).value(function(d){return 1;});
-    var arc = d3.svg.arc().outerRadius(r);
-    var arcs = vis.selectAll("g.slice")
-        .data(pie)
-        .enter()
-        .append("g")
-        .attr("class", "slice");
+var pie = d3.layout.pie().sort(null).value(function(d){return 1;});
+var arc = d3.svg.arc().outerRadius(r);
+var arcs = vis.selectAll("g.slice")
+    .data(pie)
+    .enter()
+    .append("g")
+    .attr("class", "slice");
 
-    arcs.append("path")
-        .attr("fill", function(d, i){ return color(i); })
-        .attr("d", function (d) { return arc(d); });
+arcs.append("path")
+    .attr("fill", function(d, i){ return color(i); })
+    .attr("d", function (d) { return arc(d); });
 
-    arcs.append("text").attr("transform", function(d){
-            d.innerRadius = 0;
-            d.outerRadius = r;
-            d.angle = (d.startAngle + d.endAngle)/2;
-            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
-        })
-        .attr("text-anchor", "end")
-        .text( function(d, i) {
-            return data[i].label;
-        });
-
-    container.on("click", spin);
-
-    function spin(d) {
-        container.on("click", null);
-        var spinSound = document.getElementById("spinSound");
-        spinSound.play();
-        if (oldpick.length == data.length) {
-            console.log("done");
-            container.on("click", null);
-            document.getElementById("restartButton").style.display = "block";
-            document.getElementById("restartButton").addEventListener("click", function() {
-            location.reload();
+arcs.append("text").attr("transform", function(d){
+        d.innerRadius = 0;
+        d.outerRadius = r;
+        d.angle = (d.startAngle + d.endAngle)/2;
+        return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
+    })
+    .attr("text-anchor", "end")
+    .text( function(d, i) {
+        return data[i].label;
     });
-            return;
-        }
-        var ps = 360 / data.length,
-            pieslice = Math.round(1440 / data.length),
-            rng = Math.floor((Math.random() * 1440) + 360);
-            
-        rotation = (Math.round(rng / ps) * ps);
+
+container.on("click", spin);
+
+function spin(d) {
+    container.on("click", null);
+    var spinSound = document.getElementById("spinSound");
+    spinSound.play();
+    if (oldpick.length == data.length) {
+        console.log("done");
+        container.on("click", null);
+        document.getElementById("restartButton").style.display = "block";
+        document.getElementById("restartButton").addEventListener("click", function() {
+            location.reload();
+        });
+        return;
+    }
+    var ps = 360 / data.length,
+        pieslice = Math.round(1440 / data.length),
+        rng = Math.floor((Math.random() * 1440) + 360);
         
-        picked = Math.round(data.length - (rotation % 360) / ps);
-        picked = picked >= data.length ? (picked % data.length) : picked;
-        if (oldpick.indexOf(picked) !== -1) {
-            d3.select(this).call(spin);
-            return;
-        } else {
-            oldpick.push(picked);
-        }
-        rotation += 90 - Math.round(ps / 2);
-        vis.transition()
-            .duration(5000)
-            .attrTween("transform", rotTween)
-            .each("end", function () {
-                d3.select(".slice:nth-child(" + (picked + 1) + ") path")
-                    .attr("fill", "#111");
-                var selectedQuestion = data[picked].question;
-                d3.select("#question h1")
-                    .text(selectedQuestion);
-                oldrotation = rotation;
+    rotation = (Math.round(rng / ps) * ps);
+    
+    picked = Math.round(data.length - (rotation % 360) / ps);
+    picked = picked >= data.length ? (picked % data.length) : picked;
+    if (oldpick.indexOf(picked) !== -1) {
+        d3.select(this).call(spin);
+        return;
+    } else {
+        oldpick.push(picked);
+    }
+    rotation += 90 - Math.round(ps / 2);
+    vis.transition()
+        .duration(5000)
+        .attrTween("transform", rotTween)
+        .each("end", function () {
+            d3.select(".slice:nth-child(" + (picked + 1) + ") path")
+                .attr("fill", "#111");
+            var selectedQuestion = data[picked].question;
+            d3.select("#question h1")
+                .text(selectedQuestion);
+            oldrotation = rotation;
 
-                var questionSound = document.getElementById("questionSound");
-                questionSound.play();
-          
-                var historyTable = d3.select("#history tbody");
-                historyTable.append("tr")
-                    .html("<td>" + data[picked].label + "</td><td>" + selectedQuestion + "</td>");
-          
-                container.on("click", spin);
+            var questionSound = document.getElementById("questionSound");
+            questionSound.play();
 
-                Swal.fire({
-                  title: 'Pregunta seleccionada:',
-                  text: selectedQuestion,
-                  icon: 'info',
-                  confirmButtonText: 'Entendido'
-                });
+            // Imprimir historial en la consola
+            console.log("Pregunta seleccionada:", selectedQuestion);
+
+            container.on("click", spin);
+
+            Swal.fire({
+                title: 'Pregunta seleccionada:',
+                text: selectedQuestion,
+                icon: 'info',
+                confirmButtonText: 'Entendido'
             });
-    }
+        });
+}
 
-    svg.append("g")
-        .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
-        .append("path")
-        .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
-        .style({"fill":"black"});
+svg.append("g")
+    .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
+    .append("path")
+    .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
+    .style({"fill":"black"});
 
-    container.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .attr("r", 60)
-        .style({"fill":"white","cursor":"pointer"});
+container.append("circle")
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", 60)
+    .style({"fill":"white","cursor":"pointer"});
 
-    container.append("text")
-        .attr("x", 0)
-        .attr("y", 15)
-        .attr("text-anchor", "middle")
-        .text("Girar")
-        .style({"font-weight":"bold", "font-size":"30px","cursor":"pointer"});
+container.append("text")
+    .attr("x", 0)
+    .attr("y", 15)
+    .attr("text-anchor", "middle")
+    .text("Girar")
+    .style({"font-weight":"bold", "font-size":"30px","cursor":"pointer"});
 
-    function rotTween(to) {
-      var i = d3.interpolate(oldrotation % 360, rotation);
-      return function(t) {
-        return "rotate(" + i(t) + ")";
-      };
-    }
-
-    function getRandomNumbers(){
-        var array = new Uint16Array(1000);
-        var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
-        if(window.hasOwnProperty("crypto") && typeof window.crypto.getRandomValues === "function"){
-            window.crypto.getRandomValues(array);
-            console.log("works");
-        } else {
-            for(var i=0; i < 1000; i++){
-                array[i] = Math.floor(Math.random() * 100000) + 1;
-            }
-        }
-        return array;
-    }
+function rotTween(to) {
+  var i = d3.interpolate(oldrotation % 360, rotation);
+  return function(t) {
+    return "rotate(" + i(t) + ")";
+  };
+}
